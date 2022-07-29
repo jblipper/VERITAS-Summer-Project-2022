74290b63eeb7e1294c645058bc9ef314b48a621c
Justin Lipper <justinlipper@MacBook-Pro-de-Justin.local>
2022-07-29 14:31:37 -0400

    shmaker to run on cluster

---
+++ Scripts/shmakercluster.py
@@ -0,0 +1,42 @@
+import numpy as np
+import os
+with open('/raid/biggams/jlipper/Significance_Cutoff/bashtemplate.sh') as f:
+    t=f.read()
+f.close()
+with open('/raid/biggams/jlipper/Significance_Cutoff/parameters/parameters.sh') as g:
+    p=g.readlines()
+g.close()
+for i in p:
+    i2=''
+    for char in i:
+        if char != '"' and char != "'" and char != '[' and char != ']' and char != ' ' and char != '(' and char != ')':
+            i2+=char
+    ii=i2.split(',')
+    #print(ii)
+    #print(i)
+    date=ii[0]
+    data=ii[9]
+    source=ii[1]
+    tn=ii[2]
+    dp=f'/raid/reedbuck/lundym/frb/d{date}/{date}-{source}-T{tn}.csv'
+    op='/raid/biggams/jlipper/Significance_Cutoff/data'
+    er=data
+    ss=16
+    nc=16
+    tr=250
+    bt=ii[4]
+    et=ii[5]
+    sr=1/1200
+    wo=ii[7]
+    wd=ii[3]
+    nb=ii[8]
+
+
+    bashfile=f'{t} -dp {dp} -op {op} -er {er} -tn {tn} -ss {ss} -nc {nc} -tr {tr} -bt {bt} -et {et} -sr {sr} -wo {wo} -wd {wd} -nb {nb}'
+    fl=open(f'/raid/biggams/jlipper/Significance_Cutoff/bashfiles/bashfile_Run{data}-T{tn}.sh','w')
+    fl.write(bashfile)
+    fl.close()
+    os.system(f'sbatch /raid/biggams/jlipper/Significance_Cutoff/bashfiles/bashfile_Run{data}-T{tn}.sh')
+
+#print(t)
+#print(p)
